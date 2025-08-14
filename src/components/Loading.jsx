fdd090b3c4c00cf5611a0a2a36f7467a5faa4b9c
Devanshu Tiwaskar <devanshutiwaskar@gmail.com>
2025-08-15 01:04:24 +0530

    Add 'ldrs' dependency and update Loading component to use ldrs grid

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,23 @@
+import 'ldrs/react/Ring.css'
+
+
+
+
+
+import { grid } from 'ldrs'
+grid.register()
+
+// Default values shown
+
+
+
 const Loading = () => (
   <div className="w-full h-screen flex justify-center items-center bg-gray-50">
-    <h1 className="text-6xl font-bold animate-pulse text-blue-600">Loading...</h1>
+    <h1 className="text-6xl font-bold animate-pulse text-blue-600"><l-grid
+  size="60"
+  speed="1.5" 
+  color="black" 
+></l-grid></h1>
   </div>
 );
 
